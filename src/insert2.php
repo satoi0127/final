7b75fdb86f8940ab76ce9708e55823cf3dff106e
satoi0127 <148302675+satoi0127@users.noreply.github.com>
2024-01-11 12:07:58 +0900

    k

--- src/insert2.php
+++ src/insert2.php
@@ -15,14 +15,6 @@
     $sql=$pdo->prepare('insert into Camera(name, maker_id, category_id, path) values (?,?,?,?)');
         $sql->execute([$_POST['name'],$_POST['maker'],$_POST['category'],$_POST['path']]);
         echo'<h2>追加が完了しました</h2>';
-        echo'<table border="1" cellpadding="10"cellspacing="0">';
-            echo'<tr>';
-            echo'<td>',$_POST['name'],'</td>';
-            echo'<td>',$_POST['maker_name'],'</td>';
-            echo'<td>',$_POST['category_name'],'</td>';
-            echo'<td><img src="img/'.$_POST['path'].'" width="60px" height="60px"></td>';
-            echo'</tr>';
-        echo'</table>';
 ?>
     
 
