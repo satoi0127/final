5820d600f310804c5e8a8c13eb1e9318a5046142
satoi0127 <148302675+satoi0127@users.noreply.github.com>
2024-01-16 11:37:11 +0900

    j

--- src/index.php
+++ src/index.php
@@ -6,12 +6,16 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 </head>
 <body>
+    <br>
     <div class="main">
     <h1>カメラ</h1>
+    <br>
+    <div class="menu">
     <button onclick="location.href='all.php'">一覧</button>
     <button onclick="location.href='insert.php'">登録</button>
     <button onclick="location.href='update.php'">更新</button>
     <button onclick="location.href='delete.php'">削除</button>
+    </div>
     </div>
 </body>
 </html>
